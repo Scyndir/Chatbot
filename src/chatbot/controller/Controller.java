18a51b0311a16f0b74e478e480df1cff4006966e
Scyndir <michaelwatts22@gmail.com>
2021-09-20 09:56:12 -0600

    made chatbot files

---
+++ src/chatbot/controller/Controller.java
@@ -0,0 +1,20 @@
+package chatbot.controller;
+
+import java.util.Scanner;
+
+public class Controller
+{
+	private Scanner input;
+	
+	
+	public Controller()
+	{
+		this.input = new Scanner(System.in);
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+}
