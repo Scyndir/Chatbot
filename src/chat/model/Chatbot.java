da76902863ae82dbee168ae193462269495f6c1e
Scyndir <michaelwatts22@gmail.com>
2021-09-22 09:54:32 -0600

    made chatbot slightly functional

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -4,7 +4,30 @@ public class Chatbot
 {
 	private String name;
 	
+	public Chatbot(String name)
+	{
+		this.name = name;
+	}
+	public String processText(String text)
+	{
+		String response = "You said: ";
+		
+		response += text + "\n";
+		
+		
+		return response;
+	}
 	
+	
+	
+	@Override
+	public String toString()
+	{
+		String description = "I am a Chatbot called: " + name + "\n";
+		description += "My favourite topic is: N/A";
+		
+		return description;
+	}
 	public String getName()
 	{
 		return this.name;
--- src/chatbot/controller/Controller.java
+++ src/chatbot/controller/Controller.java
@@ -1,20 +1,44 @@
 package chatbot.controller;
 
 import java.util.Scanner;
+import chat.model.Chatbot;
 
 public class Controller
 {
 	private Scanner input;
+	private Chatbot myChatbot;
 	
 	
 	public Controller()
 	{
 		this.input = new Scanner(System.in);
+		this.myChatbot = new Chatbot("boi");
 	}
 	
 	public void start()
 	{
+		System.out.println(myChatbot);
+		System.out.println("What do you want to talk about???");
 		
+		String userText = input.nextLine();
+		
+		while (!userText.equals("quit"))
+		{
+			String response = interactWithChatbot(userText);
+			System.out.println(response);
+			System.out.println("Keep talking?");
+			userText = input.nextLine();
+		}
+	}
+	
+	public String interactWithChatbot(String text)
+	{
+		String response = "";
+		
+		response += myChatbot.processText(text);
+		
+		
+		return response;
 	}
 	
 }
