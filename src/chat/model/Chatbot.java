fa79c63df4151b908b0f446b1d25e6c0a98b1c36
Scyndir <michaelwatts22@gmail.com>
2021-09-29 09:37:24 -0600

    added two new methods and fixed up processText some more

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,29 +1,137 @@
 package chat.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Chatbot
 {
 	private String name;
+	private int greetingCount;
+	private int farewellCount;
 	
 	public Chatbot(String name)
 	{
 		this.name = name;
+		this.greetingCount = 0;
+		this.farewellCount = 4;
 	}
+	
 	public String processText(String text)
-	{
-		String response = "You said: ";
+	{	
+		String response = sayGreeting() + "\nYou said: ";
 		
 		response += text + "\n";
 		
-		response += getDate() + "\n";
+		if (text.toLowerCase().indexOf("date") >= 0)
+		{
+			response += getDate() + "\n";
+		}
 		
-		response += getTime();
+		if (text.toUpperCase().indexOf("TIME") >= 0)
+		{
+			response += getTime() + "\n";
+		}
+		
+		if (isPolitical(text))
+		{
+			response += "gross, politics\n";
+		}
+		
+		if (isPolite(text))
+		{
+			response += "How polite of you\n";
+		}
+		
+		response += sayFarewell() + "\n";
 		
 		return response;
 	}
 	
+	public String sayGreeting()
+	{
+		String greeting = "";
+		
+		ArrayList<String> greetings = new ArrayList<String>();
+		
+		greetings.add("Hi there!");
+		greetings.add("Hello");
+		greetings.add("G'day mate");
+		greetings.add("Howdy");
+		greetings.add("What's up?");
+		
+		greeting = greetings.get(greetingCount);
+		greetingCount++;
+		if (greetingCount == greetings.size())
+		{
+			greetingCount = 0;
+		}
+		
+		return greeting;
+	}
 	
+	public String sayFarewell()
+	{
+		String farewell = "";
+		
+		ArrayList<String> farewells = new ArrayList<String>();
+		
+		farewells.add("Goodbye");
+		farewells.add("Au revoir");
+		farewells.add("Do svidaniya");
+		farewells.add("cya");
+		farewells.add("Have a good day!");
+		
+		farewell = farewells.get(farewellCount);
+		farewellCount--;
+		if (farewellCount == 0)
+		{
+			farewellCount = 4;
+		}
+		
+		return farewell;
+	}
+	
+	public boolean isPolitical(String parameter)
+	{
+		boolean political = false;
+		
+		ArrayList<String> politics = new ArrayList<String>();
+		
+		politics.add("politics");
+		politics.add("republican");
+		politics.add("democrat");
+		politics.add("election");
+		politics.add("biden");
+		
+		for (String current : politics)
+		{
+			if (parameter.toLowerCase().indexOf(current) >= 0)
+			{
+				political = true;
+			}
+		}
+		
+		return political;
+	}
+	
+	public boolean isPolite(String parameter)
+	{
+		boolean polite = false;
+		
+		ArrayList<String> politeness = new ArrayList<String>();
+		
+		politeness.add("please");
+		politeness.add("thank you");
+		
+		for (String current : politeness)
+		{
+			if (parameter.toLowerCase().indexOf(current) >= 0)
+			{
+				polite = true;
+			}
+		}
+		return polite;
+	}
 	
 	@Override
 	public String toString()
