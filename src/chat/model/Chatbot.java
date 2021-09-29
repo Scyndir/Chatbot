b0946f51c732a19051a967726aafafed1c6517f5
Scyndir <michaelwatts22@gmail.com>
2021-09-29 07:44:12 -0600

    added date time stuff

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,5 +1,7 @@
 package chat.model;
 
+import java.time.LocalDateTime;
+
 public class Chatbot
 {
 	private String name;
@@ -14,6 +16,9 @@ public class Chatbot
 		
 		response += text + "\n";
 		
+		response += getDate() + "\n";
+		
+		response += getTime();
 		
 		return response;
 	}
@@ -28,8 +33,44 @@ public class Chatbot
 		
 		return description;
 	}
+	
 	public String getName()
 	{
 		return this.name;
+	}
+	
+	public String getDate()
+	{
+		String date = "The date is ";
+		
+		LocalDateTime currentDate = LocalDateTime.now();
+		
+		String day = currentDate.getDayOfWeek().toString();
+		
+		date += day + ", ";
+		
+		day = "the " + currentDate.getDayOfMonth() + " ";
+		
+		date += day + "of ";
+		
+		String month = currentDate.getMonth().toString();
+		
+		date += month + ", " + currentDate.getYear();
+		
+		return date;
+	}
+	
+	public String getTime()
+	{
+		String time = "The time is ";
+		
+		LocalDateTime currentTime = LocalDateTime.now();
+		
+		time += currentTime.getHour() + ":";
+		time += currentTime.getMinute();
+		
+		
+		
+		return time;
 	}
 }
