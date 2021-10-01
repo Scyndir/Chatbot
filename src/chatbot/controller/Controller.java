6aa25e3f8f39a664ecf82700940bda2a0ce99466
Scyndir <michaelwatts22@gmail.com>
2021-10-01 09:53:39 -0600

    made chatbot operate with popup windows instead of terminal

--- src/chatbot/controller/Controller.java
+++ src/chatbot/controller/Controller.java
@@ -2,33 +2,45 @@ package chatbot.controller;
 
 import java.util.Scanner;
 import chat.model.Chatbot;
+import chat.view.Popup;
 
 public class Controller
 {
 	private Scanner input;
 	private Chatbot myChatbot;
-	
+	private Popup view; 
 	
 	public Controller()
 	{
 		this.input = new Scanner(System.in);
 		this.myChatbot = new Chatbot("boi");
+		this.view = new Popup();
 	}
 	
 	public void start()
 	{
-		System.out.println(myChatbot);
-		System.out.println("What do you want to talk about???");
+		//System.out.println(myChatbot);
 		
-		String userText = input.nextLine();
+		view.displayMessage("I need coffee");
+		
+		String userText = view.askQuestion("What do you want to talk about???");
+
+		//System.out.println("What do you want to talk about???");
+		
+		//String userText = input.nextLine();
 		
 		while (!userText.equals("quit"))
 		{
 			String response = interactWithChatbot(userText);
-			System.out.println(response);
-			System.out.println("Keep talking?");
-			userText = input.nextLine();
+			//System.out.println(response);
+			view.displayMessage(response);
+			
+			//System.out.println("Keep talking?");
+			//userText = input.nextLine();
+			
+			userText = view.askQuestion("Keep Talking");
 		}
+		
 	}
 	
 	public String interactWithChatbot(String text)
