2255f9974965cc4b62077d5cec64d04bfdc2ba66
Scyndir <michaelwatts22@gmail.com>
2022-02-03 08:10:17 -0700

    initialised all the stuff

--- src/chat/view/Frame.java
+++ src/chat/view/Frame.java
@@ -1,6 +1,30 @@
 package chat.view;
 
-public class Frame
+import chat.controller.Controller;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame
 {
+	private Controller app;
+	
+	private Panel panel;
+	
+	public Frame(Controller app)
+	{
+		super();
+		this.app = app;
+		this.panel = new Panel(this.app);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(panel);
+		this.setTitle("Window");
+		this.setSize(800, 600);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 
 }
